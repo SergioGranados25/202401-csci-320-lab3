2f1b75ffd1a84c05ed3bab313db4c94056d651d0
sergiogranados25 <sergiosaid0206@gmail.com>
2023-11-20 03:29:17 +0000

    read

--- lab3.c
+++ lab3.c
@@ -7,31 +7,25 @@ extern int** sudoku_board;
 int* worker_validation;
 
 int** read_board_from_file(char* filename){
-    FILE *fp = NULL;
+    FILE *fp = fopen(filename, "r"); 
     int** board = NULL;
-
-    // replace this comment with your code
-    fp = fopen(filename, "r");
-    
-    int** sudoku_board = (int**)malloc(sizeof(int*)*ROW_SIZE);
-    
-    for(int row = 0; row < ROW_SIZE; row++){
-        sudoku_board[row] = (int*)malloc(sizeof(int)*COL_SIZE);
+// replace this comment with your code
+    sudoku_board=(int**)malloc(sizeof(int*)*ROW_SIZE);
+    for (int row=0;row<ROW_SIZE;row++) {
+        sudoku_board[row]=(int*)malloc(sizeof(int)*COL_SIZE);
     }
-
-    for (int i = 0; i<ROW_SIZE*COL_SIZE;i++){
-        fscanf(fp, "%d," , &sudoku_board[i/9][i%9]);
+    for (int i=0; i<ROW_SIZE*COL_SIZE; i++) {
+        fscanf(fp, "%d,", &sudoku_board[i/9][i%9]);
     }
-
     return sudoku_board;
 }
 
 
-int is_board_valid(){
-    pthread_t* tid;  /* the thread identifiers */
+
+int is_board_valid() {
+    pthread_t tid[27];  /* the thread identifiers */
     pthread_attr_t attr;
-    param_struct* parameter;
-    
-    // replace this comment with your code
-    
+    param_struct* params = (param_struct*)malloc(sizeof(param_struct) * 27);
+    int thread_count = 0;
+
 }
